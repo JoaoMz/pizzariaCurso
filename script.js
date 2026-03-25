b83f72fb9649978a92d239d16916bbb708448336
Joao Martins <joaomartins102009@hotmail.com>
2026-03-25 10:22:28 -0300

    Adicionando img/preco/desc atraves do JS

--- script.js
+++ script.js
@@ -12,9 +12,22 @@ pizzaJson.map((item, index)=>{
     let pizzaItem = qs('.models .pizza-item').cloneNode(true);
    //preencher infos pizza item
 
+   //preencher imagem
+   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+
+
+   //preco das pizzas
+   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+   
+
+   //nome das pizzas
    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
    
-   qs('.pizza-area').append(pizzaItem);
+   //descricao das pizzas
+   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
+   
+    //add os dados
+    qs('.pizza-area').append(pizzaItem);
 
 });
