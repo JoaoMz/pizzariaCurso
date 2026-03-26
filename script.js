1fc4ac5cbb6fd7d1be2671ae2cac62e13fb49858
Joao Martins <joaomartins102009@hotmail.com>
2026-03-26 15:22:42 -0300

    Criando função botão do cancelar.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const qsAll = (el)=>{
     return document.querySelectorAll(el);
 }
 
+//listagem das pizzas
 pizzaJson.map((item, index)=>{
     
     let pizzaItem = qs('.models .pizza-item').cloneNode(true);
@@ -37,9 +38,12 @@ pizzaJson.map((item, index)=>{
         e.preventDefault(); //previne o comportamento padrao do link
 
         //capturar o datakey que esta na classe pizza item dentro do html
+        //closest -> pega o elemento pai mais proximo
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         qs('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         qs('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        qs('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+
 
         //abrir modal
         qs('.pizzaWindowArea').style.opacity = 0;
@@ -48,6 +52,17 @@ pizzaJson.map((item, index)=>{
         setTimeout(()=>{
             qs('.pizzaWindowArea').style.opacity = 1;
         },200);
+
+        //preencher os tamanhos pizzas
+        qsAll('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+
+        });
+
+        qs('.pizzaBig img').src = item.img;
+        
+        
         
    });
 
@@ -56,3 +71,17 @@ pizzaJson.map((item, index)=>{
 
 
 });
+
+
+// Eventos do Modal
+
+function closeModal(){
+    qs('.pizzaWindowArea').style.opacity = 0;
+    //função para timer
+    setTimeout(()=>{
+        qs('.pizzaWindowArea').style.display = 'none';
+    },500);
+}
+    //selecionando os botoes cancelar
+    //o foreach executa todo o array um por vez (2 cancels)
+    qsAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{item.addEventListener('click', closeModal)});
